73d6aea13578b30be245f039e56fbb9d3f528361
Meng-Chieh Lin <anton850117food1@gmail.com>
2019-09-23 14:18:34 -0400

    Add files via upload

---
+++ MutiClient/MutiClient/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace MutiClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Client Off";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(621, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Client On";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(35, 53);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(494, 410);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint_1);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(582, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(134, 29);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(533, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 18);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Please key in your name!";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(621, 97);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 41);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Confirm";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(533, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Your Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(559, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(559, 321);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 18);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Timeleft";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 492);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "MC";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ MutiClient/MutiClient/Form1.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace MutiClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient clientSocket = null;
+        int port = 12345, range = 20, flag = 0, test = 0, first = 0, timecount = 0;
+        int ambiguous = 5, paintx = 0, painty = 0, left = 30, epic = 0;
+        bool connect = false;
+        BackgroundWorker bgwClientsend, bgwClientread;
+        string msg = "----";
+        const int BOARDSIZE = 10;
+        const int BOARDLENGTH = 330;
+        int[,] chessMap = new int[BOARDSIZE, BOARDSIZE];
+        string control = "----";
+        string username = "";
+        string txt = ".txt";
+        Graphics g;
+        Bitmap bmp1, bmp2 = new Bitmap(@"C:\FFOutput\newboard.bmp", true);
+        SolidBrush blackBrush = new SolidBrush(Color.Black);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        System.Timers.Timer time, timer;
+        Thread thread, thread2;
+        public delegate void MyInvoke(string param1);
+        public delegate void sInvoke(string param2);
+
+        public Form1()
+        {
+            InitializeComponent();
+            set();
+        }
+        public void set()
+        {
+            InitialChess();
+            time = new System.Timers.Timer(1000);
+            timer = new System.Timers.Timer(1000);
+        }
+        private void pictureBox1_Paint_1(object sender, PaintEventArgs e)
+        {
+            bmp1 = bmp2;
+            pictureBox1.Image = bmp1;
+            g = Graphics.FromImage(pictureBox1.Image);
+            if (flag == 1)
+            {
+                g.FillEllipse(whiteBrush, paintx * 30 - 10, painty * 25 - 10, range, range);
+                chessMap[paintx - 1, painty - 1] = 1;
+                flag = 0;
+            }
+            else if (flag == 2)
+            {
+                g.FillEllipse(blackBrush, paintx * 30 - 10, painty * 25 - 10, range, range);
+                chessMap[paintx - 1, painty - 1] = 2;
+                flag = 0;
+            }
+            bmp2 = bmp1;
+        }
+        public void InitialChess()
+        {
+            for (int i = 0; i < BOARDSIZE; i++)
+            {
+                for (int j = 0; j < BOARDSIZE; j++)
+                {
+                    chessMap[i, j] = 0;
+                }
+            }
+            //this.pictureBox1.Invalidate();
+        }
+        public void DrawBoard(Graphics g)
+        {
+            Pen p = new Pen(Brushes.Black, 3.0f);
+            for (int i = 0; i <= BOARDSIZE; i++)
+            {
+                g.DrawLine(p, new Point(0, (i + 1) * 25), new Point(BOARDLENGTH, (i + 1) * 25));
+            }
+            for (int i = 0; i <= BOARDSIZE; i++)
+            {
+                g.DrawLine(p, new Point((i + 1) * 30, 0), new Point((i + 1) * 30, BOARDLENGTH));
+            }
+        }
+        private void pictureBox1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            if (test != 2) return;
+
+            int xx = e.X, yy = e.Y;
+            int row = xx / 30;
+            int col = yy / 25;
+            int ambx = xx % 30;
+            int amby = yy % 25;
+            int xmax = 30 - ambiguous;
+            int ymax = 25 - ambiguous;
+
+            if (ambx <= ambiguous && amby <= ambiguous)
+            {
+                if (xx < 30 || yy < 25 || xx > 300 || yy > 250) { }
+                else drawpoint(row-1, col-1);
+            }
+            else if (ambx <= ambiguous && amby >= ymax)
+            {
+                if (xx < 30 || yy < 25 || xx > 300 || yy > 250) { }
+                else drawpoint(row-1, col);
+            }
+            else if (ambx >= xmax && amby <= ambiguous)
+            {
+                if (xx < 30 || yy < 25 || xx > 300 || yy > 250) { }
+                else drawpoint(row, col-1);
+            }
+            else if (ambx >= xmax && amby >= ymax)
+            {
+                if (xx < 30 || yy < 25 || xx > 300 || yy > 250) { }
+                else drawpoint(row, col);
+            }
+        }
+        public void drawpoint(int x, int y)
+        {
+            if (chessMap[x , y] == 0)
+            {
+               
+                paintx = x + 1;
+                painty = y + 1;
+                flag = 2;
+                test = 1;
+                chessMap[x , y ] = 2;
+                pictureBox1.Refresh();
+                package();
+            }
+        }
+        public void package()
+        {
+            label1.Text = "Wait for your opponent!";
+            label1.Refresh();
+            if (epic == 1)
+            {
+                epic = 0;
+                bgwClientsend.RunWorkerAsync();
+            }
+            else
+            {
+                changecontrol(1, paintx - 1);
+                changecontrol(2, painty - 1);
+                bgwClientsend.RunWorkerAsync();
+            }
+        }
+        public void changecontrol(int pos, int num)
+        {
+            char[] chars = control.ToCharArray();
+            string str = Convert.ToString(num);
+            char a = Convert.ToChar(str);
+            chars[pos] = a;
+            control = new string(chars);
+        }
+        public void Client()
+        {
+            clientSocket = new TcpClient();
+            try
+            {
+                clientSocket.Connect("127.0.0.1", port);
+            }
+            catch (Exception ex)
+            {
+                label1.Text = "Sever not found";
+                clientSocket = null;
+                return;
+            }
+            label1.Text = "Connect to Server";
+            label1.Refresh();
+            timer.Elapsed += OnTimedEvent2;
+            time.Elapsed += OnTimedEvent;
+            time.Start();
+
+            connect = true;
+            Intbgw();
+            control = username + txt;
+            bgwClientsend.RunWorkerAsync();
+            bgwClientread.RunWorkerAsync();
+        }
+        public void Intbgw()
+        {
+            bgwClientsend = new BackgroundWorker();
+            bgwClientread = new BackgroundWorker();
+            bgwClientsend.DoWork += new DoWorkEventHandler(bgwClientDoSend);
+            bgwClientread.DoWork += new DoWorkEventHandler(bgwClientDoRead);
+            bgwClientread.RunWorkerCompleted += new RunWorkerCompletedEventHandler(ReadCom);
+            bgwClientsend.RunWorkerCompleted += new RunWorkerCompletedEventHandler(SendCom);
+        }
+        public void bgwClientDoSend(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                NetworkStream clientStream = clientSocket.GetStream();
+                string ss = control;
+                control = "----";
+                byte[] outStream = Encoding.ASCII.GetBytes(ss);
+                clientStream.Write(outStream, 0, outStream.Length);
+                clientStream.Flush();
+            }
+            catch(Exception ex)
+            {
+                //Console.WriteLine(ex.ToString());
+            }
+
+        }
+        public void bgwClientDoRead(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                NetworkStream netStream = clientSocket.GetStream();
+                byte[] inStream = new byte[1000];
+                netStream.Read(inStream, 0, inStream.Length);
+                msg = Encoding.ASCII.GetString(inStream);
+                netStream.Flush();
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.ToString());
+            }
+        }
+        public void ReadCom(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null && !e.Cancelled)
+            {
+                
+                char[] chars = msg.ToCharArray();
+                msg = "----";
+                int a = 0, b = 1, c = 3;
+                if (chars[a] != '-') controller(1, chars);
+                if (chars[b] != '-') controller(2, chars);
+                if (chars[c] != '-') controller(3, chars);
+            }
+        }
+        public void SendCom(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null && !e.Cancelled)
+            {
+                timer.Stop();
+                left = 30;
+                if (first == 1) { control = "----"; first = 0; }
+                else bgwClientread.RunWorkerAsync();
+            }
+        }
+        public void UpdateForm2(string param2)
+        {
+            left--;
+            label5.Text = "Timeleft: " + left + param2;
+            label5.Refresh();
+            if (left == 0)
+            {
+                test = 1;
+                char[] chararr = control.ToCharArray();
+                chararr[1] = 'G';
+                chararr[2] = 'G';
+                control = new string(chararr);
+                epic = 1;
+                package();
+            }
+        }
+        public void DoWork2()
+        {
+            sInvoke mi2 = new sInvoke(UpdateForm2);
+            Invoke(mi2, new Object[] { " seconds" });
+        }
+        public void OnTimedEvent2(object source, System.Timers.ElapsedEventArgs e)
+        {
+            thread2 = new Thread(new ThreadStart(DoWork2));
+            thread2.Start();
+        }
+        public void UpdateForm(string param1)
+        {
+            timecount++;
+            label4.Text = "Total: " + timecount + param1;
+            label4.Refresh();
+        }
+        public void DoWork()
+        {
+            MyInvoke mi = new MyInvoke(UpdateForm);
+            Invoke(mi, new Object[] { " seconds" });
+        }
+        public void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            thread = new Thread(new ThreadStart(DoWork));
+            thread.Start();
+        }
+        public void controller(int num, char[] schar)
+        {
+            if (num == 1)
+            {
+                if (schar[0] == '1')
+                {
+                    //MessageBox.Show("MC: You go after Server!");
+                    label1.Text = "Wait for your opponent!";
+                    label1.Refresh();
+                    test = 1;
+                    bgwClientread.RunWorkerAsync();
+
+                }
+                else if (schar[0] == '2')
+                {
+                    //MessageBox.Show("MC: You go first!");
+                    label1.Text = "Your turn!";
+                    label1.Refresh();
+                    test = 2;
+                    timer.Start();
+                }
+            }
+            else if (num == 2)
+            {
+                paintx = schar[1] - '0' + 1;
+                painty = schar[2] - '0' + 1;
+                if (schar[1] == 'G' && schar[2] == 'G')
+                {
+                    test = 2;
+                    label1.Text = "Your turn!";
+                    label1.Refresh();
+                    timer.Start();
+                }
+                else if (paintx < 1)
+                {
+                    test = 3;
+                    label1.Text = "You win!";
+                    label1.Refresh();
+                    time.Stop();
+                    timer.Stop();
+                    time.Dispose();
+                    timer.Dispose();
+                    clientSocket.Close();
+                }
+                else
+                {
+                    flag = 1;
+                    chessMap[paintx - 1, painty - 1] = 1;
+                    pictureBox1.Refresh();
+                    test = 2;
+                    label1.Text = "Your turn!";
+                    label1.Refresh();
+                    timer.Start();
+                }
+                //檢驗是否分出勝負
+            }
+            else if (num == 3)
+            {
+                if(schar[3] == '3' || schar[3] == '5')
+                {
+                    test = 3;
+                    label1.Text = "You lose!";
+                    label1.Refresh();
+                    time.Stop();
+                    timer.Stop();
+                    time.Dispose();
+                    timer.Dispose();
+                    clientSocket.Close();
+
+                }
+                else if(schar[3] == '4' || schar[3] == '6')
+                {
+                    test = 3;
+                    label1.Text = "You win!";
+                    label1.Refresh();
+                    time.Stop();
+                    timer.Stop();
+                    time.Dispose();
+                    timer.Dispose();
+                    clientSocket.Close();
+                }
+            }
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Client();
+            if(connect)button3.Enabled = false;
+            button3.Refresh();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            username = textBox1.Text;
+            label3.Text = "Your Name: " + username;
+            label3.Refresh();
+            textBox1.Enabled = false;
+            button4.Enabled = false;
+            first = 1;
+        }
+    }
+}
